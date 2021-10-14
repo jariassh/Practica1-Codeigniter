1e5caa8a3750ff8bf4535c671bbc417e0895891c
Jonathan Arias Hernandez <informesarias@gmail.com>
2021-10-14 17:20:21 -0500

    Proyecto con view home terminado con texto, imagenes y video ilustrativos

---
+++ public/modules/home.container.php
@@ -0,0 +1,224 @@
+<div class="row mb-1">
+	<div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+		<div class="carousel-indicators">
+			<button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active"></button>
+			<button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1"></button>
+			<button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2"></button>
+		</div>
+		<div class="carousel-inner">
+			<div class="carousel-item active">
+				<div class="overlay-image" style="background-image:url(<?php base_url(); ?>public/resources/img/slide1.jpg);"></div>
+				<div class="container container-carousel">
+					<h1>Example Headline</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia magnam molestiae at dolore nemo quod, adipisci praesentium, beatae nihil ducimus unde itaque expedita veritatis modi quos enim eveniet facilis totam.</p>
+					<a href="#" class="btn btn-lg btn-primary">
+						Sign up today
+					</a>
+				</div>
+			</div>
+			<div class="carousel-item">
+				<div class="overlay-image" style="background-image:url(<?php base_url(); ?>public/resources/img/slide2.jpg);"></div>
+				<div class="container container-carousel">
+					<h1>Example Headline</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia magnam molestiae at dolore nemo quod, adipisci praesentium, beatae nihil ducimus unde itaque expedita veritatis modi quos enim eveniet facilis totam.</p>
+					<a href="#" class="btn btn-lg btn-primary">
+						Sign up today
+					</a>
+				</div>
+			</div>
+			<div class="carousel-item">
+				<div class="overlay-image" style="background-image:url(<?php base_url(); ?>public/resources/img/slide3.jpg);"></div>
+				<div class="container container-carousel">
+					<h1>Example Headline</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia magnam molestiae at dolore nemo quod, adipisci praesentium, beatae nihil ducimus unde itaque expedita veritatis modi quos enim eveniet facilis totam.</p>
+					<a href="#" class="btn btn-lg btn-primary">
+						Sign up today
+					</a>
+				</div>
+			</div>
+		</div>
+		<!-- Left and right controls/icons -->
+		<button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+			<span class="carousel-control-prev-icon"></span>
+		</button>
+		<button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+			<span class="carousel-control-next-icon"></span>
+		</button>
+	</div>
+</div>
+<div class="row mt-1 mb-1">
+	<div class="col-5 d-flex justify-content-center mt-1 mt-1 pr-3 video">
+
+		<iframe class="w-100 h-100" src="https://www.youtube.com/embed/y4z2FZHZnSc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	</div>
+	<div class="col-7 mt-1 mt-1 pr-3 text-justify">
+		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum quas quia aut aperiam blanditiis, autem deserunt rem quidem facilis eveniet omnis, consectetur officiis quibusdam repudiandae maxime? Animi dolorum molestiae rerum.
+			Consequatur explicabo, ratione labore doloribus odio molestiae voluptatibus dolor tempore accusamus quo sed pariatur minima quisquam cumque unde veritatis nulla! Provident, molestiae. Quos autem ex laborum exercitationem sed qui accusantium.
+			Blanditiis dolor sed unde laudantium minima natus quod porro? Magni non vero perspiciatis natus minus veritatis dolorem vitae necessitatibus voluptatum consequatur excepturi praesentium qui et commodi, accusamus, magnam quasi nam?
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius rem temporibus dignissimos deserunt, dolorum laboriosam commodi distinctio fugiat mollitia voluptatum, accusamus illo corrupti autem. Nam laborum odit vitae eius eos?
+			Similique nihil rem voluptatibus voluptates fugiat, possimus provident nam numquam ex fuga omnis explicabo voluptas iure, vel sint, eius quasi voluptate. Voluptas omnis veritatis hic temporibus ipsum autem amet! Libero!
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere deserunt similique. </p>
+	</div>
+</div>
+<div class="row mt-1 mb-1">
+	<div id="galery" class="container">
+		<div class="row">
+			<h2 class="text-center">Our Gallery</h2>
+			<p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, quisquam quo aliquam iusto voluptatum quia veniam assumenda libero.</p>
+		</div>
+		<div class="row">
+			<div class="col-3">
+				<a href="#!" data-bs-toggle="modal" data-bs-target="#modalImage1">
+					<img class="rounded" src="<?php base_url(); ?>public/resources/img/student1.jpg" alt="Galery Images">
+				</a>
+			</div>
+			<div class="col-3">
+				<a href="#!" data-bs-toggle="modal" data-bs-target="#modalImage2">
+					<img class="rounded" src="<?php base_url(); ?>public/resources/img/student2.jpg" alt="Galery Images">
+				</a>
+			</div>
+			<div class="col-3">
+				<a href="#!" data-bs-toggle="modal" data-bs-target="#modalImage3">
+					<img class="rounded" src="<?php base_url(); ?>public/resources/img/student3.jpg" alt="Galery Images">
+				</a>
+			</div>
+			<div class="col-3">
+				<a href="#!" data-bs-toggle="modal" data-bs-target="#modalImage4">
+					<img class="rounded" src="<?php base_url(); ?>public/resources/img/student4.jpg" alt="Galery Images">
+				</a>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-3">
+				<a href="#!" data-bs-toggle="modal" data-bs-target="#modalImage5">
+					<img class="rounded" src="<?php base_url(); ?>public/resources/img/student5.jpg" alt="Galery Images">
+				</a>
+			</div>
+			<div class="col-3">
+				<a href="#!" data-bs-toggle="modal" data-bs-target="#modalImage6">
+					<img class="rounded" src="<?php base_url(); ?>public/resources/img/student6.jpg" alt="Galery Images">
+				</a>
+			</div>
+			<div class="col-3">
+				<a href="#!" data-bs-toggle="modal" data-bs-target="#modalImage7">
+					<img class="rounded" src="<?php base_url(); ?>public/resources/img/student7.jpg" alt="Galery Images">
+				</a>
+			</div>
+			<div class="col-3">
+				<a href="#!" data-bs-toggle="modal" data-bs-target="#modalImage8">
+					<img class="rounded" src="<?php base_url(); ?>public/resources/img/student8.jpg" alt="Galery Images">
+				</a>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-3">
+				<a href="#!" data-bs-toggle="modal" data-bs-target="#modalImage9">
+					<img class="rounded" src="<?php base_url(); ?>public/resources/img/student9.jpg" alt="Galery Images">
+				</a>
+			</div>
+			<div class="col-3">
+				<a href="#!" data-bs-toggle="modal" data-bs-target="#modalImage10">
+					<img class="rounded" src="<?php base_url(); ?>public/resources/img/student10.jpg" alt="Galery Images">
+				</a>
+			</div>
+			<div class="col-3">
+				<a href="#!" data-bs-toggle="modal" data-bs-target="#modalImage11">
+					<img class="rounded" src="<?php base_url(); ?>public/resources/img/student11.jpg" alt="Galery Images">
+				</a>
+			</div>
+			<div class="col-3">
+				<a href="#!" data-bs-toggle="modal" data-bs-target="#modalImage12">
+					<img class="rounded" src="<?php base_url(); ?>public/resources/img/student12.jpg" alt="Galery Images">
+				</a>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- Start Modal Gallery -->
+<div tabindex="-1" aria-labelledby="modalImage1" aria-hidden="true" class="modal fade" id="modalImage1">
+	<div class="modal-dialog modal-lg modal-dialog-centered">
+		<div class="modal-content">
+			<img class="rounded" src="<?php base_url(); ?>public/resources/img/student1.jpg" alt="Galery Images">
+		</div>
+	</div>
+</div>
+<div tabindex="-1" aria-labelledby="modalImage2" aria-hidden="true" class="modal fade" id="modalImage2">
+	<div class="modal-dialog modal-lg modal-dialog-centered">
+		<div class="modal-content">
+			<img class="rounded" src="<?php base_url(); ?>public/resources/img/student2.jpg" alt="Galery Images">
+		</div>
+	</div>
+</div>
+<div tabindex="-1" aria-labelledby="modalImage3" aria-hidden="true" class="modal fade" id="modalImage3">
+	<div class="modal-dialog modal-lg modal-dialog-centered">
+		<div class="modal-content">
+			<img class="rounded" src="<?php base_url(); ?>public/resources/img/student3.jpg" alt="Galery Images">
+		</div>
+	</div>
+</div>
+<div tabindex="-1" aria-labelledby="modalImage4" aria-hidden="true" class="modal fade" id="modalImage4">
+	<div class="modal-dialog modal-lg modal-dialog-centered">
+		<div class="modal-content">
+			<img class="rounded" src="<?php base_url(); ?>public/resources/img/student4.jpg" alt="Galery Images">
+		</div>
+	</div>
+</div>
+<div tabindex="-1" aria-labelledby="modalImage5" aria-hidden="true" class="modal fade" id="modalImage5">
+	<div class="modal-dialog modal-lg modal-dialog-centered">
+		<div class="modal-content">
+			<img class="rounded" src="<?php base_url(); ?>public/resources/img/student5.jpg" alt="Galery Images">
+		</div>
+	</div>
+</div>
+<div tabindex="-1" aria-labelledby="modalImage6" aria-hidden="true" class="modal fade" id="modalImage6">
+	<div class="modal-dialog modal-lg modal-dialog-centered">
+		<div class="modal-content">
+			<img class="rounded" src="<?php base_url(); ?>public/resources/img/student6.jpg" alt="Galery Images">
+		</div>
+	</div>
+</div>
+<div tabindex="-1" aria-labelledby="modalImage7" aria-hidden="true" class="modal fade" id="modalImage7">
+	<div class="modal-dialog modal-lg modal-dialog-centered">
+		<div class="modal-content">
+			<img class="rounded" src="<?php base_url(); ?>public/resources/img/student7.jpg" alt="Galery Images">
+		</div>
+	</div>
+</div>
+<div tabindex="-1" aria-labelledby="modalImage8" aria-hidden="true" class="modal fade" id="modalImage8">
+	<div class="modal-dialog modal-lg modal-dialog-centered">
+		<div class="modal-content">
+			<img class="rounded" src="<?php base_url(); ?>public/resources/img/student8.jpg" alt="Galery Images">
+		</div>
+	</div>
+</div>
+<div tabindex="-1" aria-labelledby="modalImage9" aria-hidden="true" class="modal fade" id="modalImage9">
+	<div class="modal-dialog modal-lg modal-dialog-centered">
+		<div class="modal-content">
+			<img class="rounded" src="<?php base_url(); ?>public/resources/img/student9.jpg" alt="Galery Images">
+		</div>
+	</div>
+</div>
+<div tabindex="-1" aria-labelledby="modalImage10" aria-hidden="true" class="modal fade" id="modalImage10">
+	<div class="modal-dialog modal-lg modal-dialog-centered">
+		<div class="modal-content">
+			<img class="rounded" src="<?php base_url(); ?>public/resources/img/student10.jpg" alt="Galery Images">
+		</div>
+	</div>
+</div>
+<div tabindex="-1" aria-labelledby="modalImage11" aria-hidden="true" class="modal fade" id="modalImage11">
+	<div class="modal-dialog modal-lg modal-dialog-centered">
+		<div class="modal-content">
+			<img class="rounded" src="<?php base_url(); ?>public/resources/img/student11.jpg" alt="Galery Images">
+		</div>
+	</div>
+</div>
+<div tabindex="-1" aria-labelledby="modalImage12" aria-hidden="true" class="modal fade" id="modalImage12">
+	<div class="modal-dialog modal-lg modal-dialog-centered">
+		<div class="modal-content">
+			<img class="rounded" src="<?php base_url(); ?>public/resources/img/student12.jpg" alt="Galery Images">
+		</div>
+	</div>
+</div>
+<!-- End Modal Gallery -->
